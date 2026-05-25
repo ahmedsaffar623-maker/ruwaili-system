1731e3faffb92a633239ef7b420113c96f067c9b
ahmedsaffar623-maker <ahmedsaffar623@gmail.com>
2026-05-25 03:08:20 +0300

    Add basic HTML structure for index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-# ruwaili-system
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <title>نظام الرويعي</title>
+</head>
+<body>
+    <h1>نظام مؤسسة الرويعي - تم التشغيل بنجاح!</h1>
+</body>
+</html>
+
